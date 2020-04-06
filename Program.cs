6ce149a65c42d3d2d7ac37b91109f5a3f64daa2a
William Goode <wgoode3@gmail.com>
2020-04-06 16:29:39 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fundamental13
+{
+    class Program
+    {
+
+
+        static void Print1To255() 
+        {
+            for(int i=0; i<256; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Print1To255();
+        }
+    }
+}
